f9e5f17013d5740cdf0f6fbf57709c43546c26e9
Muhammed Said Cakir <muhammedsaidckr@gmail.com>
2022-10-02 23:51:05 +0300

    truffle initialization and tests are created

---
+++ truffle/contracts/Fundraiser.sol
@@ -0,0 +1,68 @@
+pragma solidity >= 0.4.0 < 0.9.0;
+
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract Fundraiser is Ownable {
+	struct Donation {
+		uint256 value;
+		uint256 date;
+	}
+	mapping(address => Donation[]) private _donations;
+
+
+	string public name; 
+	string public url; 
+	string public imageURL;
+	string public description;
+	address payable public beneficiary;
+
+	constructor(
+		string memory _name,
+		string memory _url,
+		string memory _imageURL, 
+		string memory _description,
+		address payable _beneficiary,
+		address _custodian 
+	) public {
+		name = _name;
+		url = _url;
+		imageURL = _imageURL;
+		description = _description;
+		beneficiary = _beneficiary;
+		transferOwnership(_custodian);
+	}
+
+	function setBeneficiary(address payable _beneficiary) public onlyOwner {
+		beneficiary = _beneficiary; 
+	}
+
+	function myDonationsCount() public view returns(uint256) {
+		return _donations[msg.sender].length; 
+	}
+
+	function donate() public payable { 
+		Donation memory donation = Donation({ 
+			value: msg.value,
+			date: block.timestamp
+		});
+		_donations[msg.sender].push(donation); 
+	}
+
+	function myDonations() public view returns(
+		uint256[] memory values,
+		uint256[] memory dates
+	)
+	{
+		uint256 count = myDonationsCount();
+		values = new uint256[](count); 
+		dates = new uint256[](count); 
+
+		for (uint256 i = 0; i < count; i++) {
+			Donation storage donation = _donations[msg.sender][i];
+			values[i] = donation.value;
+			dates[i] = donation.date;
+		}
+
+		return (values, dates); 
+	}
+}
---
+++ truffle/test/fundraiser_test.js
@@ -0,0 +1,108 @@
+const FundraiserContract = artifacts.require("Fundraiser");
+
+contract("Fundraiser",accounts => { 
+	let fundraiser;
+	const name = "Beneficiary name";
+	const url = "beneficiaryname.org";
+	const imageURL = "https://placekitten.com/600/350";
+	const description = "Beneficiary description";
+	const beneficiary = accounts[1];
+	const owner = accounts[0]; 
+        beforeEach(async () => {
+        	fundraiser = await FundraiserContract.new(
+                	name,
+                        url,
+                        imageURL,
+                        description,
+                        beneficiary,
+                        owner
+          	);
+        });
+
+	describe("Initialization", () => {
+		it("gets the beneficiary name", async () => {
+			const actual = await fundraiser.name();
+			assert.equal(actual, name, "names should match"); 
+		});
+
+		it("gets the beneficiary url", async () => {
+			const actual = await fundraiser.url(); 
+			assert.equal(actual, url, "url should match"); 
+		});
+
+		it("gets the beneficiary image url", async() => {
+			const actual = await fundraiser.imageURL();
+			assert.equal(actual, imageURL, "imageURL should match");
+		}); 
+
+		it("gets the beneficiary description", async () => {
+			const actual = await fundraiser.description();
+			assert.equal(actual, description, "description should match"); 
+		});
+		it("gets the beneficiary", async() => {
+			const actual = await fundraiser.beneficiary();
+			assert.equal(actual, beneficiary, "beneficiary address should match"); 
+		}); 
+
+		it("gets the owner", async() => {
+			const actual = await fundraiser.owner();
+			assert.equal(actual, owner, "bios address should match"); 
+		});
+	});
+
+	describe("setBeneficiary", () => {
+		const newBeneficiary = accounts[2]; 
+
+		it("updated beneficiary when called by owner account", async () => {
+			await fundraiser.setBeneficiary(newBeneficiary, {from:owner}); 
+			const actualBeneficiary = await fundraiser.beneficiary(); 
+			assert.equal(actualBeneficiary, newBeneficiary, "beneficiaries should match");
+		}); 
+
+		it("throws an error when called from a non-owner account", async () => {
+			try {
+				await fundraiser.setBeneficiary(newBeneficiary, {from:accounts[3]}); 
+				assert.fail("withdraw was not restricted to owners"); 
+			} catch(err) {
+				const expectedError = "Ownable: caller is not the owner";
+				const actualError = err.reason; 
+				assert.equal( actualError, expectedError, "should not permitted"); 
+			}
+		}); 
+	});
+
+	describe("making donations", () => {
+		const value =  web3.utils.toWei('0.0289');
+		const donor = accounts[2];
+
+		it("increases myDonationsCount" , async () => {
+			const currentDonationsCount = await fundraiser.myDonationsCount(
+				{from: donor} 
+			);
+
+			await fundraiser.donate({from: donor, value});
+
+			const newDonationsCount = await fundraiser.myDonationsCount(
+				{from: donor} 
+			);
+
+			assert.equal(
+				1,
+				newDonationsCount - currentDonationsCount, 
+				"myDonationsCount should increment by 1");
+		});
+		it("includes donation in myDonations", async () => {
+			await fundraiser.donate({from: donor, value});
+			const {values, dates} = await fundraiser.myDonations(
+				{from:donor}
+			);
+			assert.equal(
+				value, 
+				values[0],
+				"values should match"
+			);
+
+			assert(dates[0], "date should be present"); 
+		});
+	});
+});
